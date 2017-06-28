79e7354ef36534bb49f12a8ea83d74dc426a752c
Tony Pitale <tpitale@gmail.com>
2017-06-28 17:04:58 -0500

    Update mix file to create hex package

--- mix.exs
+++ mix.exs
@@ -7,25 +7,28 @@ defmodule Legato.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     source_url: "https://github.com/tpitale/legato-ex",
+     description: description(),
+     package: package(),
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger, :httpoison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp description do
+    "Google Analytics Reporting API v4 in Elixir"
+  end
+
+  defp package do
+    [
+      maintainers: ["Tony Pitale"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/tpitale/legato-ex"}
+    ]
+  end
+
   defp deps do
     [
       {:httpoison, "~> 0.9.0"},
